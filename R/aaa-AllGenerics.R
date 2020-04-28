eba6cb979dd739fb3663eba38ff0d633ff1a2ee3
andreamrau <andrea.rau@gmail.com>
2020-04-28 13:54:15 +0200

    Add S4 classes and generics

---
+++ R/aaa-AllGenerics.R
@@ -0,0 +1,23 @@
+#' @rdname padma
+#' @export
+setGeneric("padma", function(object, ...) standardGeneric("padma"))
+
+#' @rdname padmaHelpers
+#' @export
+setGeneric("pathway_name", function(object, ...) standardGeneric("pathway_name"))
+
+#' @rdname padmaHelpers
+#' @export
+setGeneric("MFA_results", function(object, ...) standardGeneric("MFA_results"))
+
+#' @rdname padmaHelpers
+#' @export
+setGeneric("ngenes", function(object, ...) standardGeneric("ngenes"))
+
+#' @rdname padmaHelpers
+#' @export
+setGeneric("imputed_genes", function(object, ...) standardGeneric("imputed_genes"))
+
+#' @rdname padmaHelpers
+#' @export
+setGeneric("remvoed_genes", function(object, ...) standardGeneric("removed_genes"))
